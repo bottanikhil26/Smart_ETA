6e509f3b3526492c5faae6c9c14046a0cb87dda0
Nikhil Botta <nikhil@Nikhils-MacBook-Pro.local>
2025-04-27 17:53:25 +0530

    Logging and expection are ready

---
+++ src/smarteta/exception/exception.py
@@ -0,0 +1,26 @@
+import sys
+from src.smarteta.logging import logger
+class SmartetaException(Exception):
+    """Base class for all exceptions raised by Smarteta."""
+    def __init__(self,error_message,error_detail:sys):
+        """
+        Args:
+            error_message (str): The error message to be displayed.
+            error_detail (sys): The error detail object.
+        """
+        self.error_message = error_message
+        _,_,exc_tb = error_detail.exc_info()
+
+        self.line_number = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        """
+        Returns the string representation of the exception.
+        """
+        return f"Error occurred in python script name [{0}] at line number [{1}] error message [{2}]".format(
+            self.file_name,
+            self.line_number,
+            self.error_message
+        )
+    
